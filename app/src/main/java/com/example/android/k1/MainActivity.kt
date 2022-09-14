ad3f944ca68bfac39e270ae15c250d404dfed0c2
PratyushFunde <fundepratyush42@gmail.com>
2022-09-14 22:15:19 +0530

    firstcommit

---
+++ app/src/main/java/com/example/android/k1/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.android.k1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var  diceImage :ImageView
+    lateinit var diceImage2:ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        diceImage=findViewById(R.id.dice_image)
+        diceImage2=findViewById(R.id.dice_image2)
+
+
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{rollDice()}
+        val resetButton: Button=findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { resetDice() }
+
+
+    }
+    private fun resetDice(){
+        diceImage.setImageResource(R.drawable.dice_1)
+        diceImage2.setImageResource(R.drawable.dice_1)
+    }
+    private fun rollDice(){
+
+            diceImage.setImageResource(getDiceImage())
+            diceImage2.setImageResource(getDiceImage())
+
+
+
+    }
+
+       private fun getDiceImage():Int{
+           val randomInt=(1..6).random()
+           return when(randomInt){
+               1->R.drawable.dice_1
+               2->R.drawable.dice_2
+               3->R.drawable.dice_3
+               4->R.drawable.dice_4
+               5->R.drawable.dice_5
+               else->R.drawable.dice_6
+           }
+       }
+
+
+    }
+
+
+
+
+
